02c92e54ea885e4df40d2bd6fa9a726b19719de2
joshcarp <joshuacarpeggiani@gmail.com>
2020-10-19 14:57:00 +1100

    aoidhas

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/sandertv/mcwss"
+	"os"
 )
 
 func main() {
 	// Create a new server using the default configuration. To use specific configuration, pass a *wss.Config{} in here.
 	server := mcwss.NewServer(&mcwss.Config{
 		HandlerPattern: "/ws",
-		Address:        ":80",
+		Address:        ":"+os.Getenv("PORT"),
 	})
 
 	server.OnConnection(func(player *mcwss.Player){
