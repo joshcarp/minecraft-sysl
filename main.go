18b3019aaaab22093a560e9638a167df5cb2feea
joshcarp <joshuacarpeggiani@gmail.com>
2020-10-20 22:23:06 +1100

    adkjn

--- main.go
+++ main.go
@@ -64,6 +64,7 @@ func MOTD(player *mcwss.Player) {
 func DoSysl(p *mcwss.Player, m *sysl.Module) {
 	p.Position(func(pos mctype.Position) {
 		for _, app := range m.Apps {
+			Fill(p, pos, -20, -2, -20, 20, 15, 20, "chicken")
 			Summonpos(p, pos, "chicken", fmt.Sprintf("%s:service:%s", app.Name.String(), app.Name.String()))
 		}
 	})
